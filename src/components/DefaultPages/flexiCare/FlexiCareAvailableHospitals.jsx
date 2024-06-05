0f123bcc210af7d444a327da71528bef3849e589
KhurramSagoo <34654560+KhurramSagoo@users.noreply.github.com>
2024-06-05 22:16:34 +0500

    hospitals continue

--- src/components/DefaultPages/flexiCare/FlexiCareAvailableHospitals.jsx
+++ src/components/DefaultPages/flexiCare/FlexiCareAvailableHospitals.jsx
@@ -1,34 +1,34 @@
 import React, { useState } from "react";
 import { states } from "@/components/db/state";
-
-export const hospitals = [
-  {
-    name: "ZenCare Prime Hospital",
-    type: "Hospital",
-    plan: "ZenCare Prime",
-    state: "lagos",
-  },
-  {
-    name: "ZenCare Plus Clinic",
-    type: "Clinic",
-    plan: "ZenCare Plus",
-    state: "abuja",
-  },
-  {
-    name: "Flexi Care Eye Center",
-    type: "Eye Care",
-    plan: "Flexi Care",
-    state: "enugu",
-  },
-  {
-    name: "Flexi Care Mini Dental",
-    type: "Dental Clinic",
-    plan: "Flexi Care Mini",
-    state: "kano",
-  },
-  { name: "ZenCare Pharmacy", type: "Pharmacy", plan: "ZenCare", state: "oyo" },
-  // Add more dummy data as needed
-];
+import { hospitals } from "@/components/db/flexiCareHospitalsData";
+// export const hospitals = [
+//   {
+//     name: "ZenCare Prime Hospital",
+//     type: "Hospital",
+//     plan: "ZenCare Prime",
+//     state: "lagos",
+//   },
+//   {
+//     name: "ZenCare Plus Clinic",
+//     type: "Clinic",
+//     plan: "ZenCare Plus",
+//     state: "abuja",
+//   },
+//   {
+//     name: "Flexi Care Eye Center",
+//     type: "Eye Care",
+//     plan: "Flexi Care",
+//     state: "enugu",
+//   },
+//   {
+//     name: "Flexi Care Mini Dental",
+//     type: "Dental Clinic",
+//     plan: "Flexi Care Mini",
+//     state: "kano",
+//   },
+//   { name: "ZenCare Pharmacy", type: "Pharmacy", plan: "ZenCare", state: "oyo" },
+//   // Add more dummy data as needed
+// ];
 
 const FlexiCareAvailableHospitals = () => {
   const [selectedPlan, setSelectedPlan] = useState("all");
@@ -68,7 +68,7 @@ const FlexiCareAvailableHospitals = () => {
   });
 
   return (
-    <div className="lg:py-[120px] md:py-[100px] sm:py-[80px] py-[50px] px-4 bg-[#f8fcfb] w-full lg:px-16 h-full">
+    <div className="lg:py-[120px] md:py-[100px] sm:py-[80px] py-[50px] px-4 bg-[#f8fcfb] w-full lg:px-16 h-full flex items-center justify-center flex-col">
       <div className="my-4 px-4 flex items-center justify-center flex-col flex-wrap w-full">
         <p className="my-4 text-secondary md:text-3xl sm:text-2xl lg:text-5xl text-xl font-extrabold text-wrap">
           View our available hospitals
@@ -78,18 +78,30 @@ const FlexiCareAvailableHospitals = () => {
         </p>
       </div>
 
+      {/* plans tags*/}
+
+      <div className="flex items-center justify-center w-full flex-wrap">
+        {plans.map((plan, index) => (
+          <button
+            onClick={() => setSelectedPlan(plan)}
+            className={`m-2 p-2 border rounded ${
+              selectedPlan === plan ? "bg-primary text-white" : ""
+            }`}
+            key={index}
+          >
+            {plan}
+          </button>
+        ))}
+      </div>
+
       <div className="flex flex-wrap justify-center items-center mb-4">
-        <select
-          value={selectedPlan}
-          onChange={handleFilterChange(setSelectedPlan)}
+        <input
+          type="text"
+          placeholder="Search by name"
+          value={searchKeyword}
+          onChange={(e) => setSearchKeyword(e.target.value)}
           className="m-2 p-2 border rounded"
-        >
-          {plans.map((plan) => (
-            <option key={plan} value={plan}>
-              {plan}
-            </option>
-          ))}
-        </select>
+        />
         <select
           value={selectedType}
           onChange={handleFilterChange(setSelectedType)}
@@ -112,25 +124,25 @@ const FlexiCareAvailableHospitals = () => {
             </option>
           ))}
         </select>
-        <input
-          type="text"
-          placeholder="Search by name"
-          value={searchKeyword}
-          onChange={(e) => setSearchKeyword(e.target.value)}
-          className="m-2 p-2 border rounded"
-        />
       </div>
 
-      <div className="flex flex-wrap justify-center">
+      <div
+        className="flex flex-wrap justify-center  items-center md:w-1/2 w-full
+       h-96 overflow-y-scroll"
+      >
         {filteredHospitals.length > 0 ? (
           filteredHospitals.map((hospital, index) => (
             <div
               key={index}
-              className="bg-white p-4 m-2 border rounded shadow-md w-full md:w-1/3 lg:w-1/4"
+              className="bg-white p-4 m-2 border rounded shadow-md w-full flex items-center justify-between"
             >
-              <h3 className="text-xl font-bold">{hospital.name}</h3>
-              <p>{hospital.plan}</p>
-              <p>{hospital.type}</p>
+              <div>
+                <h3 className="text-base text-primary font-medium">
+                  {hospital.name}
+                </h3>
+                {/* <p>{hospital.plan}</p> */}
+                {/* <p>{hospital.type}</p> */}
+              </div>
               <p>{hospital.state}</p>
             </div>
           ))
