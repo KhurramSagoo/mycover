7b77da4ac45da52fe8b328addcf868035d90144c
KhurramSagoo <34654560+KhurramSagoo@users.noreply.github.com>
2024-06-06 14:41:13 +0500

    travel, flexicare are ready

--- src/components/DefaultPages/flexiCare/FlexiCareAvailableHospitals.jsx
+++ src/components/DefaultPages/flexiCare/FlexiCareAvailableHospitals.jsx
@@ -1,17 +1,14 @@
-import React, { useState } from "react";
-// import {} from "@/components/db/state";
+import { useState } from "react";
 import { hospitals, states } from "@/components/db/flexiCareHospitalsData";
 import SearchIcon from "@mui/icons-material/Search";
 import CloseIcon from "@mui/icons-material/Close";
+
 const FlexiCareAvailableHospitals = () => {
   const [selectedPlan, setSelectedPlan] = useState("ZenCare Prime");
   const [selectedType, setSelectedType] = useState("clinic");
   const [selectedState, setSelectedState] = useState("all");
   const [searchKeyword, setSearchKeyword] = useState("");
-
   const [showCloseIcon, setShowCloseIcon] = useState(false);
-
-  // console.log(searchKeyword);
 
   const plans = [
     "ZenCare Prime",
@@ -21,61 +18,61 @@ const FlexiCareAvailableHospitals = () => {
     "ZenCare",
   ];
 
-  // console.log(hospitals);
-
   const types = ["Clinic", "Hospital", "Eye Care", "Dental Clinic", "Pharmacy"];
 
   const handleFilterChange = (setter) => (event) => {
     setter(event.target.value);
   };
 
-  const filteredHospitals = hospitals.filter((hospital) => {
-    return (
-      ((selectedPlan === "ZenCare Prime" &&
-        hospital.plan === "ZenCare Prime") ||
-        (selectedPlan === "ZenCare Plus" && hospital.plan === "ZenCare Plus") ||
-        (selectedPlan === "Flexi Care" && hospital.plan === "Flexi Care") ||
-        (selectedPlan === "Flexi Care Mini" &&
-          hospital.plan === "Flexi Care Mini") ||
-        (selectedPlan === "ZenCare" && hospital.plan === "ZenCare")) &&
-      ((selectedType === "clinic" && types.includes(hospital.type)) ||
-        hospital.type === selectedType) &&
-      (selectedState === "all" || hospital.state === selectedState) &&
-      // hospital.name.toLowerCase().includes(searchKeyword.toLowerCase())
-      hospital.name.toLowerCase().includes(searchKeyword.toLowerCase())
-    );
-  });
-
+  // filtered hospitals
   // const filteredHospitals = hospitals.filter((hospital) => {
   //   return (
   //     ((selectedPlan === "ZenCare Prime" &&
   //       hospital.plan === "ZenCare Prime") ||
+  //       (selectedPlan === "ZenCare Plus" && hospital.plan === "ZenCare Plus") ||
+  //       (selectedPlan === "Flexi Care" && hospital.plan === "Flexi Care") ||
+  //       (selectedPlan === "Flexi Care Mini" &&
+  //         hospital.plan === "Flexi Care Mini") ||
   //       (selectedPlan === "ZenCare" && hospital.plan === "ZenCare")) &&
-  //     (selectedType === "Clinic" || hospital.type === selectedType) &&
-  //     (selectedState === "all" || hospital.state === selectedState) &&
+  //     ((selectedType === "clinic" && types.includes(hospital.type)) ||
+  //       hospital.type === selectedType) &&
+  //     ((selectedState === "all" && hospital.state.toLowerCase()) ||
+  //       hospital.state.toLowerCase() === selectedState) &&
   //     hospital.name.toLowerCase().includes(searchKeyword.toLowerCase())
   //   );
   // });
+  const filteredHospitals = hospitals.filter((hospital) => {
+    const matchesPlan =
+      hospital.plan.toLowerCase() === selectedPlan.toLowerCase();
+    const matchesType =
+      hospital.type.toLowerCase() === selectedType.toLowerCase();
+    const matchesState =
+      selectedState === "All" ||
+      hospital.state.toLowerCase() === selectedState.toLowerCase();
+    const matchesSearch = hospital.name
+      .toLowerCase()
+      .includes(searchKeyword.toLowerCase());
+    return matchesPlan && matchesType && matchesState && matchesSearch;
+  });
 
   return (
-    <div className="lg:py-[120px] md:py-[100px] sm:py-[80px] py-[50px] px-4 bg-[#f8fcfb] w-full lg:px-16 h-full flex items-center justify-center flex-col">
+    <div className=" px-4 bg-[#f8fcfb] w-full lg:px-16  lg:h-screen h-auto flex items-center justify-center flex-col">
       <div className="my-4 px-4 flex items-center justify-center flex-col flex-wrap w-full">
         <p className="my-4 text-secondary md:text-3xl sm:text-2xl lg:text-5xl text-xl font-extrabold text-wrap">
           View our available hospitals
         </p>
-        <p className="my-4 text-gray-600 text-sm md:text-base font-medium text-wrap w-2/3 ">
+        <p className="my-4 text-gray-600 text-sm md:text-base font-medium text-wrap text-center">
           Search by Plan, Location, or Hospital name
         </p>
       </div>
 
-      {/* plans tags*/}
-
+      {/* Plan tags */}
       <div className="flex items-center justify-center w-full flex-wrap">
         {plans.map((plan, index) => (
           <button
             onClick={() => setSelectedPlan(plan)}
             className={`m-2 p-2 text-base font-medium ${
-              selectedPlan === plan ? " text-primary   " : " text-gray-700"
+              selectedPlan === plan ? " text-primary" : " text-gray-700"
             }`}
             key={index}
           >
@@ -84,17 +81,31 @@ const FlexiCareAvailableHospitals = () => {
         ))}
       </div>
 
-      <div className="flex flex-wrap justify-center items-center mb-4 w-full border border-primary px-5 py-2 rounded-md">
-        <div className=" w-3/5">
-          <SearchIcon className=" text-gray-400 cursor-pointer" />
+      <div className="flex flex-wrap justify-center items-center mb-4 lg:w-1/2 w-full border border-primary px-5 py-2 rounded-md">
+        <div className=" w-3/5 relative">
+          <SearchIcon
+            className=" text-gray-400 cursor-pointer absolute left-2 top-3"
+            onClick={() => setSearchKeyword(searchKeyword)}
+          />
           <input
             type="text"
             placeholder="Search by name"
             value={searchKeyword}
-            onChange={(e) => setSearchKeyword(e.target.value)}
-            className=" p-2 rounded w-4/5  outline-none "
+            onChange={(e) => {
+              setSearchKeyword(e.target.value);
+              setShowCloseIcon(e.target.value.length > 0);
+            }}
+            className=" p-2 rounded w-4/5 pl-10 outline-none "
           />
-          <CloseIcon className=" text-gray-400 cursor-pointer" />
+          {showCloseIcon && (
+            <CloseIcon
+              className=" text-gray-400 cursor-pointer absolute right-2 top-3"
+              onClick={() => {
+                setSearchKeyword("");
+                setShowCloseIcon(false);
+              }}
+            />
+          )}
         </div>
         <select
           value={selectedType}
@@ -120,54 +131,42 @@ const FlexiCareAvailableHospitals = () => {
         </select>
       </div>
 
+      {/* Number of hospitals found */}
       <div className="flex justify-between  items-start lg:w-1/2 w-full px-5">
         <div className=" text-primary font-semibold text-base ">
-          {/* hospitals searched */}
           {filteredHospitals.length} Hospitals
         </div>
         <div className=" text-primary font-semibold text-base ">
-          {/* hospitals searched */}
-          <p>State </p>
+          <p>State</p>
         </div>
       </div>
 
-      <div
-        className="flex flex-wrap justify-start  items-start lg:w-1/2 w-full
-       h-96 overflow-y-scroll"
-      >
+      {/* List of filtered hospitals */}
+      <div className="flex flex-wrap justify-start  items-start lg:w-1/2 w-full h-96 overflow-y-scroll">
         {filteredHospitals.length > 0 ? (
           filteredHospitals.map((hospital, index) => (
             <div
               key={index}
-              className=" hover:bg-gray-200 rounded-md p-4 m-2   w-full flex items-center justify-between"
+              className=" hover:bg-gray-200 rounded-md p-4 m-2 w-full flex items-center justify-between"
             >
               <div className=" flex items-start justify-start flex-col">
-                {/* no of hospitals searched */}
                 <div className=" text-primary font-semibold text-base ">
-                  {/* hospitals searched */}
+                  {/* Hospital name */}
                 </div>
-
-                <h3 className="text-base text-gray-600  font-medium">
+                <h3 className="text-base text-gray-600 font-medium">
                   {hospital.name}
                 </h3>
-                {/* <p>{hospital.plan}</p> */}
-                {/* <p>{hospital.type}</p> */}
               </div>
               <div className=" flex items-start justify-start flex-col">
-                {/* no of hospitals searched */}
-
-                <h3 className="text-base text-gray-600  font-medium">
+                <h3 className="text-base text-gray-600 font-medium">
                   {hospital.state}
                 </h3>
-                {/* <p>{hospital.plan}</p> */}
-                {/* <p>{hospital.type}</p> */}
               </div>
             </div>
           ))
         ) : (
           <div className=" text-primary font-semibold text-base ">
-            {/* 0 {hospital.name} */}
-            {/* hospitals searched */}
+            {/* No hospitals found */}
           </div>
         )}
       </div>
--- src/components/DefaultPages/flexiCare/FlexiCareEverything.jsx
+++ src/components/DefaultPages/flexiCare/FlexiCareEverything.jsx
@@ -34,45 +34,47 @@ const FlexiCareEveryThing = () => {
 
       {/* Cards */}
       <div className="flex items-center justify-center my-6 w-full h-full">
-        <div className="flex items-start justify-start w-full overflow-x-auto gap-5 h-full">
+        <div className="flex items-start justify-start w-full overflow-x-auto gap-5 h-full py-5">
           {tabData.map((item, index) => (
             <div
               key={index}
-              className="flex items-start justify-start gap-10 shadow-custom-lg flex-col min-w-[405px] min-h-[630px] px-5 py-10 rounded-md h-full"
+              className="flex items-start justify-between gap-10 shadow-custom-lg flex-col lg:min-w-[400px] min-w-[300px] lg:min-h-[585px] min-h-[530px] px-5 py-5 rounded-md h-full"
             >
-              <div className="flex items-start justify-start flex-col w-full gap-5">
-                <p className="text-2xl text-primary font-medium">
-                  {item.title}
-                </p>
-                <p className="text-3xl text-secondary font-semibold">
-                  {item.heading}{" "}
-                  <span className="text-xl text-gray-500 font-medium">
-                    /month
-                  </span>
-                </p>
-              </div>
-              <div className="w-full flex items-center justify-center flex-col gap-5 h-full">
-                {item.card &&
-                  item.card.map((card, cardIndex) => (
-                    <div
-                      className="flex items-center justify-start w-full gap-5"
-                      key={cardIndex}
-                    >
+              <div className=" flex items-start justify-start flex-col">
+                <div className="flex items-start justify-start flex-col w-full gap-2">
+                  <p className="lg:text-2xl text-base md:text-xl text-primary font-medium">
+                    {item.title}
+                  </p>
+                  <p className="lg:text-3xl text-base sm:text-2xl text-secondary font-semibold">
+                    {item.heading}{" "}
+                    <span className="text-xl text-gray-500 font-medium">
+                      /month
+                    </span>
+                  </p>
+                </div>
+                <div className="w-full flex items-center justify-center flex-col gap-5 h-full my-5">
+                  {item.card &&
+                    item.card.map((card, cardIndex) => (
                       <div
-                        className="flex items-center justify-center w-4 h-4 rounded-full bg-[#ecfdf3]"
+                        className="flex items-center justify-start w-full gap-5"
                         key={cardIndex}
                       >
-                        <img src={card.image} alt="" className="" />
+                        <div
+                          className="flex items-center justify-center w-4 h-4 rounded-full bg-[#ecfdf3]"
+                          key={cardIndex}
+                        >
+                          <img src={card.image} alt="" className="" />
+                        </div>
+                        <p className="lg:text-base text-xs text-gray-700 font-medium">
+                          {card.text}
+                        </p>
                       </div>
-                      <p className="text-base text-gray-700 font-medium">
-                        {card.text}
-                      </p>
-                    </div>
-                  ))}
+                    ))}
+                </div>
               </div>
 
               {/* Button */}
-              <div className="w-full flex items-end justify-end flex-col gap-5">
+              <div className="w-full flex items-center justify-center flex-col gap-5">
                 <button
                   className="w-full py-3 px-4 bg-primary text-sm font-bold text-white rounded-md max-w-[565px] h-[46px]"
                   onClick={handleGetCovered}
